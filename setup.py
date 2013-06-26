c79e09134f11ed146c37bc96d635d30f9ad1fbc9
Martin Zimmermann <info@posativ.org>
2013-06-27 00:35:21 +0200

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='twirss',
+    version='0.1',
+    author='Martin Zimmermann',
+    author_email='info@posativ.org',
+    license='WTFPL',
+    install_requires='tweepy',
+    scripts=['twirss.py']
+)
